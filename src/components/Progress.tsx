fea5711fc8473f8964ac219dd09cf828b92c0d95
yurii.pidlisnyi <yuriipidlisnyi2021@gmail.com>
2024-07-20 23:07:39 +0300

    feat: UI changes on light mode

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -4,7 +4,7 @@ type Props = {
     width: number
 }
 const Progress: FC<Props> = ({width}) =>
-    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 sticky z-10 top-0 md:top-1">
+    <div className="w-full bg-gray-300 rounded-full h-2.5 dark:bg-gray-700 sticky z-10 top-0 ">
         <div className="bg-blue-600 h-2.5 rounded-full" style={{
             width: `${width}%`,
         }}></div>
