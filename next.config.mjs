5038e0039dd15a3a0023f481566da1a49b2d10e1
uvarovalexandr <uvarov.alexandr89@gmail.com>
2024-07-20 15:20:13 +0300

    style-image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'default',
+    domains: [
+      'localhost',
+      'imgproxy.amomama.de'
+    ],
+  },
 };
 
 export default nextConfig;
