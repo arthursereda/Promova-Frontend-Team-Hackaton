61487b5c1a0e8b93ec3be36cdd1b934da0088404
yurii.pidlisnyi <yuriipidlisnyi2021@gmail.com>
2024-07-21 00:34:15 +0300

    feat: add all bids | PES PATRON PLS HELP

--- src/test-fix.js
+++ src/test-fix.js
@@ -1,4 +1,4 @@
-const div_1_sizes = [
+export const div_1_sizes = [
   [300, 250],
   [320, 100],
   [320, 50],
@@ -8,15 +8,110 @@ export const loadAd = (name) => {
   window.googletag = window.googletag || { cmd: [] };
 
   googletag.cmd.push(() => {
-    // Define an ad slot for div with id "banner-ad".
     googletag
-      .defineSlot('/21668216007/amomama.de_intext_10', div_1_sizes, name)
-      .addService(googletag.pubads());
+        .defineSlot('/21668216007/amomama.de_intext_10', div_1_sizes, name)
+        .addService(googletag.pubads());
 
-    // Enable the PubAdsService.
     googletag.enableServices();
 
-    // Request and render an ad for the "banner-ad" slot.
     googletag.display(name);
   });
+};
+
+// adConfig.js
+
+export const addAdUnits = (pbjsInstance, id, div_1_sizes) => {
+  const adUnit = {
+    code: id,
+    mediaTypes: {
+      banner: {
+        sizes: div_1_sizes,
+      },
+    },
+    bids: [
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: 14049821,
+        },
+      },
+      {
+        bidder: "seedtag",
+        params: {
+          publisherId: "1422-9699-01",
+          adUnitId: "31402727",
+          placement: "inArticle",
+        },
+      },
+      {
+        bidder: "vidazoo",
+        params: {
+          cid: "638f4064ba1056ce9c0324e4",
+          pId: "59ac17c192832d0011283fe3",
+        },
+      },
+      {
+        bidder: "medianet",
+        params: {
+          cid: "8CU1XT34E",
+          crid: "928505701",
+        },
+      },
+      {
+        bidder: "amx",
+        params: {
+          tagId: "YW1vbWVkaWEtcm9uLmNvbQ",
+        },
+      },
+      {
+        bidder: "triplelift",
+        params: {
+          inventoryCode: "Amomama_DE_midarticle",
+        },
+      },
+      {
+        bidder: "triplelift",
+        params: {
+          inventoryCode: "Amomama_DE_hdx",
+        },
+      },
+      {
+        bidder: "kueezrtb",
+        params: {
+          cId: "64d4bb1f226f0c02d40867c9",
+          pId: "65lk7c192882r0011813fn9",
+        },
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "559907359",
+          delDomain: "genesis-d.openx.net",
+        },
+      },
+      {
+        bidder: "rubicon",
+        params: {
+          accountId: 13524,
+          siteId: 533320,
+          zoneId: 3264228,
+        },
+      },
+      {
+        bidder: "grid",
+        params: {
+          uid: "391046",
+        },
+      },
+      {
+        bidder: "pubmatic",
+        params: {
+          publisherId: "158654",
+          adSlot: "amomama.de/mweb_rectangle",
+        },
+      },
+    ],
+  };
+
+  pbjsInstance.addAdUnits([adUnit]);
 };
